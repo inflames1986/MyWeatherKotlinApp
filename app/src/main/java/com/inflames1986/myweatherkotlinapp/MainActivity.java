5b7908f3b8b8fa7a5b35aab318c6aced45a8ef3f
TMichael <js5859168@gmail.com>
2021-10-03 10:45:32 +0300

    Create project without Kotlin support

---
+++ app/src/main/java/com/inflames1986/myweatherkotlinapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.inflames1986.myweatherkotlinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
